30c48dc79b75f69b83286c4495f7b883f834ba02
Vanyi0924 <jia643889952@qq.com>
2022-10-13 18:27:29 +0800

    add: blog

---
+++ assets/getting-started.md.68ccdcd9.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as a,a as s}from"./app.8f2244da.js";const f=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"getting-started.md"}'),r={name:"getting-started.md"},n=s("p",null,"get start",-1),o=[n];function c(d,_,i,p,l,g){return a(),e("div",null,o)}const u=t(r,[["render",c]]);export{f as __pageData,u as default};
